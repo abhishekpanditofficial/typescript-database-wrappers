40fb7694f18a87fea60ccd3560898824feefcb1f
Abhishek Pandit <90089685+abhishekpanditofficial@users.noreply.github.com>
2023-02-23 17:12:18 +0530

    updated.

--- firebase-wrapper.ts
+++ firebase-wrapper.ts
@@ -1,111 +1,134 @@
-import { firestore } from 'firebase-admin';
+import * as admin from 'firebase-admin';
 
-type CollectionPath = string | firestore.CollectionReference;
+admin.initializeApp();
 
-interface FirestoreWrapperOptions {
+type QueryOptions = {
+  where?: [string, FirebaseFirestore.WhereFilterOp, any][];
+  orderBy?: [string, FirebaseFirestore.OrderByDirection][];
+  limit?: number;
+  startAt?: FirebaseFirestore.DocumentSnapshot;
+  endAt?: FirebaseFirestore.DocumentSnapshot;
+};
+
+type GetOneOptions = {
   errorFlag?: boolean;
-  batchSize?: number;
-}
+} & QueryOptions;
 
-function firestoreWrapper<T>(
-  collectionPath: CollectionPath,
-  { errorFlag = false, batchSize = 500 }: FirestoreWrapperOptions = {}
-) {
-  const getCollectionRef = () => {
-    if (typeof collectionPath === 'string') {
-      return firestore().collection(collectionPath);
+type PathSegment = {
+  collectionId: string;
+  docId?: string;
+};
+
+const buildQuery = (path: string, options: QueryOptions = {}) => {
+  const pathSegments: PathSegment[] = path.split('/').reduce((segments: PathSegment[], segment: string, index: number) => {
+    if (index % 2 === 0) {
+      segments.push({ collectionId: segment });
+    } else {
+      segments[segments.length - 1].docId = segment;
     }
-    return collectionPath;
-  };
-
-  const getOne = async (
-    documentPath: string,
-    options: FirestoreWrapperOptions = {}
-  ): Promise<T | null | false> => {
-    const docRef = getCollectionRef().doc(documentPath);
-    const doc = await docRef.get();
-    if (doc.exists) {
-      return doc.data() as T;
+    return segments;
+  }, []);
+  let query = admin.firestore().collection(pathSegments.shift()!.collectionId);
+  for (const { collectionId, docId } of pathSegments) {
+    if (docId) {
+      query = query.doc(docId).collection(collectionId);
+    } else {
+      query = query.collection(collectionId);
     }
-    if (options.errorFlag) {
-      throw new Error(`Document does not exist at path: ${docRef.path}`);
+  }
+  if (options.where) {
+    for (const [fieldPath, opStr, value] of options.where) {
+      query = query.where(fieldPath, opStr, value);
     }
-    return false as null | false;
-  };
-
-  const create = async (
-    documentPath: string,
-    data: T
-  ): Promise<firestore.DocumentReference> => {
-    const docRef = getCollectionRef().doc(documentPath);
-    await docRef.set(data);
-    return docRef;
-  };
-
-  const updateOne = async (
-    documentPath: string,
-    data: Partial<T>,
-    options: FirestoreWrapperOptions = {}
-  ): Promise<void> => {
-    const docRef = getCollectionRef().doc(documentPath);
-    const doc = await docRef.get();
-    if (!doc.exists) {
-      if (options.errorFlag) {
-        throw new Error(`Document does not exist at path: ${docRef.path}`);
-      }
-      return;
+  }
+  if (options.orderBy) {
+    for (const [fieldPath, direction] of options.orderBy) {
+      query = query.orderBy(fieldPath, direction);
     }
-    await docRef.update(data);
-  };
+  }
+  if (options.limit) {
+    query = query.limit(options.limit);
+  }
+  if (options.startAt) {
+    query = query.startAt(options.startAt);
+  }
+  if (options.endAt) {
+    query = query.endAt(options.endAt);
+  }
+  return query;
+};
 
-  const updateMany = async (
-    query: firestore.Query,
-    data: Partial<T>,
-    options: FirestoreWrapperOptions = {}
-  ): Promise<void> => {
-    const batchSize = options.batchSize || batchSize;
-    await query.batch(batchSize).update(data);
-  };
+const getOne = async (
+  path: string,
+  options: GetOneOptions = {}
+): Promise<FirebaseFirestore.DocumentData | null | false> => {
+  const { errorFlag = false, ...queryOptions } = options;
+  const query = buildQuery(path, queryOptions);
+  const snapshot = await query.get();
+  if (snapshot.empty) {
+    return errorFlag ? Promise.reject(new Error(`Document not found at path ${path}`)) : false;
+  }
+  return snapshot.docs[0].data();
+};
 
-  const deleteOne = async (
-    documentPath: string,
-    options: FirestoreWrapperOptions = {}
-  ): Promise<void> => {
-    const docRef = getCollectionRef().doc(documentPath);
-    const doc = await docRef.get();
-    if (!doc.exists) {
-      if (options.errorFlag) {
-        throw new Error(`Document does not exist at path: ${docRef.path}`);
-      }
-      return;
-    }
-    await docRef.delete();
-  };
+const create = async (path: string, data: FirebaseFirestore.DocumentData) => {
+  const pathSegments = path.split('/');
+  let batch = admin.firestore().batch();
+  let ref: FirebaseFirestore.DocumentReference | null = null;
+  for (let i = pathSegments.length - 1; i >= 0; i -= 2) {
+    ref = ref ? ref.collection(pathSegments[i]).doc(pathSegments[i - 1]) : admin.firestore().collection(pathSegments[i]).doc(pathSegments[i - 1]);
+    batch.set(ref, data);
+  }
+  await batch.commit();
+};
 
-  const deleteMany = async (
-    query: firestore.Query,
-    options: FirestoreWrapperOptions = {}
-  ): Promise<void> => {
-    const batchSize = options.batchSize || batchSize;
-    await query.batch(batchSize).delete();
-  };
+const getAll = async (path: string, options: QueryOptions = {}) => {
+  const query = buildQuery(path, options);
+  const snapshot = await query.get();
+  return snapshot.docs.map((doc) => doc.data());
+};
 
-  const getAll = async (
-    query: firestore.Query,
-    options: FirestoreWrapperOptions = {}
-  ): Promise<T[]> => {
-    const batchSize = options.batchSize || batchSize;
-    const docs = await query.batch(batchSize).get();
-    return docs.docs.map((doc) => doc.data() as T);
-  };
+const updateOne = async (
+  path: string,
+  options: GetOneOptions = {},
+  data: FirebaseFirestore.DocumentData
+) => {
+  const query = buildQuery(path, options);
+  const snapshot = await query.get();
+  if (snapshot.empty) {
+    throw new Error(`Document not found at path ${path}`);
+  }
+  await snapshot.docs[0].ref.update(data);
+};
 
-  return {
-    getOne,
-    create,
-    updateOne,
-    updateMany,
-    deleteOne,
-    deleteMany,
-    getAll,
-  };
-}
+const updateMany = async (
+  path: string,
+  options: QueryOptions = {},
+  data: FirebaseFirestore.DocumentData
+) => {
+  const query = buildQuery(path, options);
+  const snapshot = await query.get();
+  const batch = admin.firestore().batch();
+  snapshot.docs.forEach((doc) => batch.update(doc.ref, data));
+  await batch.commit();
+};
+
+const deleteOne = async (path: string, options: GetOneOptions = {}) => {
+  const query = buildQuery(path, options);
+  const snapshot = await query.get();
+  if (snapshot.empty) {
+    throw new Error(`Document not found at path ${path}`);
+  }
+  await snapshot.docs[0].ref.delete();
+};
+
+const deleteMany = async (path: string, options: QueryOptions = {}) => {
+  const query = buildQuery(path, options);
+  const snapshot = await query.get();
+  const batch = admin.firestore().batch();
+  snapshot.docs.forEach((doc) => batch.delete(doc.ref));
+  await batch.commit();
+};
+
+export { getOne, create, getAll, updateOne, updateMany, deleteOne, deleteMany };
+
